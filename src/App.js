c581b4fcf2ae5ea31ed30d6e48a5c72216071f11
gilbertlc <gilbert.lacasandile@gmail.com>
2021-07-31 20:01:28 +0800

    implement line graph for worldwide cases

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import "./App.css";
 import Map from "./Map";
 import Table from "./Table";
 import { sortData } from "./util";
+import LineGraph from "./LineGraph";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -104,6 +105,7 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData}></Table>
           <h3>Worldwide new cases</h3>
+          <LineGraph />
           {/* Graph */}
         </CardContent>
       </Card>
